bc406f9027854690780f26837b6630af82834899
Ngô Minh Đức <ducnm@mioapp.vn>
2023-02-28 13:07:58 +0700

    connection pool

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3001
-const db = require('./queries');s
+const db = require('./queries');
 
 app.use(bodyParser.json())
 app.use(
--- queries.js
+++ queries.js
@@ -20,7 +20,7 @@ const createUser = (request, response) => {
 }
 
 const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+  pool.query('SELECT * FROM users ORDER BY user_id ASC', (error, results) => {
     if (error) {
       throw error
     }
@@ -31,7 +31,7 @@ const getUsers = (request, response) => {
 const getUserById = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+  pool.query('SELECT * FROM users WHERE user_id = $1', [id], (error, results) => {
     if (error) {
       throw error
     }
@@ -44,7 +44,7 @@ const updateUser = (request, response) => {
   const { name, email } = request.body
 
   pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    'UPDATE users SET name = $1, email = $2 WHERE user_id = $3',
     [name, email, id],
     (error, results) => {
       if (error) {
